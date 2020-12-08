71a64b89507f0c09ba8a785fd712cca4815dc661
defenitionofreal <flavors@inbox.ru>
2020-12-08 13:50:50 +0300

    static root

--- smarturlprj/settings.py
+++ smarturlprj/settings.py
@@ -147,8 +147,8 @@ USE_TZ = True
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
